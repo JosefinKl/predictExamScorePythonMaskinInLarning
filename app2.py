dbc5c0471682c9919f5e8b12c09afd07bd258202
josefinkl <josefin.klangefjall@gmail.com>
2025-10-14 11:28:13 +0200

    first commit

---
+++ app2.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request
+from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
+import joblib 
+import numpy as np
+
+model_filename = 'exam_score.joblib'
+
+try:
+    loaded_model = joblib.load(model_filename)
+    print("Modellen laddad")
+except FileNotFoundError:
+    print("Error")
+
+#new_exam_Score = np.array([[20]])
+
+#predict = loaded_model.predict(new_exam_Score)
+
+#print(predict)
+
+
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "super-hemlig-kod"
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
+
+hashed_password_josefin = bcrypt.generate_password_hash("123456").decode('utf-8')
+hashed_password_anna = bcrypt.generate_password_hash("pasword").decode('utf-8')
+
+print(hashed_password_josefin)
+print(hashed_password_anna)
+
+users = {
+    "josefin" : hashed_password_josefin,
+    "anna" : hashed_password_anna
+}
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+   
+    stored_hashed_password = users.get(username)
+   
+    if not bcrypt.check_password_hash(stored_hashed_password, password):
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
+    
+
+# CRUD - create , read, update, delete
+
+@app.route('/', methods=['GET'])
+def get_hello():
+    return 'Hello'
+
+
+@app.route('/examScore', methods=['POST'])
+#@jwt_required()
+def examScore():
+    data = request.get_json()
+    hours_studied = data.get('hours_studied', 0)
+    new_exam_Score = np.array([[hours_studied]])
+    predict = loaded_model.predict(new_exam_Score)
+    return jsonify({'prediction': predict.tolist()})
+
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
